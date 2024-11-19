df43e1d7c5eede700d7d8b460b1430bbc1d4f5bb
Finn Carroll <carrofin@amazon.com>
2024-11-19 15:02:14 -0800

    Begin proto to search request conversion
    Signed-off-by: Finn Carroll <carrofin@amazon.com>

--- server/src/main/java/org/opensearch/grpc/services/SearchServiceImpl.java
+++ server/src/main/java/org/opensearch/grpc/services/SearchServiceImpl.java
@@ -10,9 +10,9 @@ package org.opensearch.grpc.services;
 
 import io.grpc.stub.StreamObserver;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.opensearch.action.search.SearchAction;
 import org.opensearch.client.node.NodeClient;
+import org.opensearch.core.action.ActionListener;
 
 import opensearch.proto.services.SearchServiceGrpc;
 import opensearch.protos.ExplainRequest;
@@ -22,26 +22,70 @@ import opensearch.protos.IndexSearchResponse;
 import opensearch.protos.SearchRequest;
 import opensearch.protos.SearchResponse;
 
+import static org.opensearch.grpc.services.helpers.IndexSearchRequestProtoHelper.searchRequestFromProto;
+
 public class SearchServiceImpl extends SearchServiceGrpc.SearchServiceImplBase {
-    private static final Logger logger = LogManager.getLogger(SearchServiceImpl.class);
     private final NodeClient client;
 
     public SearchServiceImpl(NodeClient client) {
         this.client = client;
     }
 
+    private static class SearchRequestActionListener implements ActionListener<org.opensearch.action.search.SearchResponse> {
+        private StreamObserver<IndexSearchResponse> respObserver = null;
+
+        public SearchRequestActionListener(StreamObserver<IndexSearchResponse> responseObserver){
+            super();
+            respObserver = responseObserver;
+        }
+
+        @Override
+        public void onResponse(org.opensearch.action.search.SearchResponse response) {
+            try {
+                IndexSearchResponse protoResponse = respToProto(response);
+                respObserver.onNext(protoResponse);
+                respObserver.onCompleted();
+            } catch (Exception e) {
+                respObserver.onError(
+                    new RuntimeException("Failed to process SearchResponse", e)
+                );
+            }
+        }
+
+        @Override
+        public void onFailure(Exception e) {
+            respObserver.onError(
+                new RuntimeException("SearchRequest task failed", e)
+            );
+        }
+    };
+
+    // TODO: Redundant with indexSearch
     @Override
-    public void search(SearchRequest request, StreamObserver<SearchResponse> responseObserver) {
-        super.search(request, responseObserver);
+    public void search(SearchRequest proto, StreamObserver<SearchResponse> responseObserver) {
+        throw new UnsupportedOperationException("Redundant search - Please use IndexSearch");
     }
 
     @Override
-    public void indexSearch(IndexSearchRequest request, StreamObserver<IndexSearchResponse> responseObserver) {
-        super.indexSearch(request, responseObserver);
+    public void indexSearch(IndexSearchRequest proto, StreamObserver<IndexSearchResponse> responseObserver) {
+        org.opensearch.action.search.SearchRequest searchReq = searchRequestFromProto(proto);
+        SearchRequestActionListener listener = new SearchRequestActionListener(responseObserver);
+        client.execute(SearchAction.INSTANCE, searchReq, listener);
     }
 
     @Override
     public void explain(ExplainRequest request, StreamObserver<ExplainResponse> responseObserver) {
+        // TODO: IMPL
         super.explain(request, responseObserver);
+    }
+
+
+
+
+
+
+    // TODO: IMPL
+    private static IndexSearchResponse respToProto(org.opensearch.action.search.SearchResponse response) {
+        return IndexSearchResponse.newBuilder().build();
     }
 }
